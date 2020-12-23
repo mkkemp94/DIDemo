1373d3e7ded7e777ae73265e2435816ed0399d67
MKemp <mkemp@accu-time.com>
2020-12-23 17:33:43 -0500

    Module has state so component must use builder() instead of create(). Pass values to module

--- app/src/main/java/com/anushka/didemo/MemoryCardModule.kt
+++ app/src/main/java/com/anushka/didemo/MemoryCardModule.kt
@@ -1,13 +1,15 @@
 package com.anushka.didemo
 
+import android.util.Log
 import dagger.Module
 import dagger.Provides
 
 @Module
-class MemoryCardModule {
+class MemoryCardModule(val memorySize: Int) {
 
     @Provides
     fun provideMemoryCard() : MemoryCard {
+        Log.i("MYTAG", "Size of memory card: $memorySize")
         return MemoryCard()
     }
 
